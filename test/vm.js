6b1366fd52a00e78118f98786a3ba31c1c535868
Adam Brady <adam@boxxen.org>
2015-05-11 21:22:14 +1000

    add more tests

--- test/vm.js
+++ test/vm.js
@@ -5,24 +5,59 @@ var CPU = require('../vm');
 
 describe("CPU", function() {
 
-  it("should work", function() {
+  describe("instructions", function() {
 
-    var program = [
-      "PSH", "5",
-      "PSH", "2",
-      "ADD"
-    ];
+    describe("PSH", function() {
 
-    var cpu = new CPU(program);
+      it("should push value to stack", function() {
 
-    assert.throws(function() {
-      cpu.run();
+        var cpu = new CPU([ "PSH", "5", "HLT" ]);
+
+        cpu.run();
+
+        assert.equal(cpu.stack[0], 5);
+
+      });
+
     });
 
-    assert.equal(program.length, cpu.IP);
-    assert.equal(cpu.stack[0], 7);
+    describe("ADD", function() {
+
+      it("should add previous 2 values from stack", function() {
+
+        var cpu = new CPU([
+          "PSH", "5",
+          "PSH", "10",
+          "ADD",
+          "HLT"
+        ]);
+
+        cpu.run();
+
+        assert.equal(cpu.stack[0], 15);
+
+      });
+
+    });
+
+    describe("POP", function() {
+
+      it("should pop value from stack", function() {
+
+        var cpu = new CPU([
+          "PSH", "5",
+          "POP",
+          "HLT"
+        ]);
+
+        cpu.run();
+
+        assert.equal(cpu.stack.length, 0);
+
+      });
+
+    });
 
   });
-
 
 });
